6dc0da2a7ad76b2f0f0cc3ac3baa9ed06a615994
MauroLondon <75399405+MauroLondon@users.noreply.github.com>
2025-06-13 11:34:19 -0400

    palm sos real time DE

---
+++ real-time/DE/helpers.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf8
+
+import re
+import os
+import datetime
+
+
+class Helpers(object):
+    def __init__(self):
+        pass
+
+    # get date current
+    def date_current(self):
+        return (datetime.datetime.now()).strftime("%m-%d-%Y")
+
+    # create new folder
+    def create_folder(self, directory_name):
+        directory = os.path.isdir(directory_name)
+        if directory == False:
+            try:
+                os.mkdir(directory_name)
+            except OSError:
+                msg = ("Creation of the directory failed")
+            else:
+                msg = ("Successfully created the directory")
+        folder_path = os.path.realpath(directory_name)
+        return folder_path
+    
+    def clean_item(self, item):
+        item = " ".join(item.split())
+        item = item.strip()
+        return item 
