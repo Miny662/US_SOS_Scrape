8c993f2cb48be12b150d96151f021ed51f84bbf3
Phill Smith <psmith1618@gmail.com>
2025-06-30 14:31:50 -0500

    call get_proxies() in BaseScraper.__init__()

--- historical-data/state_sos/base_scraper.py
+++ historical-data/state_sos/base_scraper.py
@@ -1,18 +1,21 @@
 import datetime
-from typing import Dict
+from typing import Dict, List
 
 from state_sos.util.aws import write_file_to_s3
 from state_sos.util.config import CONFIG
+from state_sos.util.proxies import get_proxies
 
 
 class BaseScraper:
     STATE: str
     state_code: str
     s3_source_key_prefix: str = "sources/sos"
+    proxies: List[Dict]
 
     def __init__(self, start_id: int = 1, end_id: int = 7800000):
         self.START_ID = start_id
         self.END_ID = end_id
+        self.proxies = get_proxies()
 
     def write_to_s3(self, item: Dict, item_id: str):
         write_file_to_s3(
