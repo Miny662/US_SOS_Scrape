7c86ee20f09f29284254d0f0e4996a7492518a93
Phill Smith <psmith1618@gmail.com>
2025-07-01 15:27:33 -0500

    add command line args for start_id, end_id in main.py

--- historical-data/main.py
+++ historical-data/main.py
@@ -5,10 +5,9 @@ from state_sos.MS.scraper import Scraper as MississippiScraper
 from state_sos.NY.scraper import Scraper as NewYorkScraper
 from state_sos.OH.scraper import Scraper as OhioScraper
 from state_sos.PA.scraper import Scraper as PennsylvaniaScraper
-from state_sos.util.config import CONFIG
 
 
-def get_scraper(state_code: str):
+def get_scraper(state_code: str, start_id: int = 119999, end_id: int = 120000):
     state_map = {
         'ms': MississippiScraper,
         # 'de': DelawareScraper,
@@ -16,15 +15,15 @@ def get_scraper(state_code: str):
         'oh': OhioScraper,
         'pa': PennsylvaniaScraper
     }
-    if CONFIG.sos_scraper_environment == 'local':
-        return state_map[state_code.lower()](start_id=119990, end_id=120000)
-    return state_map[state_code.lower()]()
+    return state_map[state_code.lower()](start_id=start_id, end_id=end_id)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--state", help="state to load", default="ms")
+    parser.add_argument("--start_id", help="beginning id", default=119999, type=int)
+    parser.add_argument("--end_id", help="ending id", default=120000, type=int)
     args = parser.parse_args()
     print(args)
-    scraper = get_scraper(args.state)
+    scraper = get_scraper(state_code=args.state, start_id=args.start_id, end_id=args.end_id)
     scraper.run()
